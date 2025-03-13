de83adee10d7e5ec8aaafa497e9f186661fe1359
SebastianVahos <vahossebas@gmail.com>
2025-03-13 14:55:15 -0500

    Agregar archivos de proyecto.

---
+++ Parcial1/Clases/clsComputador.cs
@@ -0,0 +1,76 @@
+﻿using Parcial1.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace Parcial1.Clases
+{
+    public class clsComputador
+    {
+        private VentaComputadoresItmEntities computador = new VentaComputadoresItmEntities();
+        public Computador compu { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                computador.Computadors.Add(compu);
+                computador.SaveChanges();
+                return "Equipo insertado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar el equipo: " + ex.Message;
+            }
+        }
+        public string Actualizar()
+        {
+            try
+            {
+                Computador comp = Consultar(compu.Codigo);
+                if (comp == null)
+                {
+                    return "El computador con el codigo ingresado no existe, no se puede actualizar";
+                }
+                computador.Computadors.AddOrUpdate(compu);
+                computador.SaveChanges();
+                return "Se actualizo el computador correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo actualizar el equipo: " + ex.Message;
+            }
+        }
+
+        public Computador Consultar(int Codigo)
+        {
+            return computador.Computadors.FirstOrDefault(c => c.Codigo == Codigo);
+        }
+
+        public List<Computador> ConsultarTodos()
+        {
+            return computador.Computadors.ToList();
+        }
+
+        public string Eliminar(int Codigo)
+        {
+            try
+            {
+                Computador comp = Consultar(Codigo);
+                if (comp == null)
+                {
+                    return "El computador con el codigo ingresado no existe, no se puede eliminar";
+                }
+                computador.Computadors.Remove(comp);
+                computador.SaveChanges();
+                return "Se elimino el computador correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "No se pudo eliminar el computador: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ Parcial1/Controllers/ComputadorsController.cs
@@ -0,0 +1,53 @@
+﻿using Parcial1.Clases;
+using Parcial1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Parcial1.Controllers
+{
+    [RoutePrefix("api/Computador")]
+    public class ComputadorsController : ApiController
+    {
+        [HttpGet]
+        [Route("ConsultarXCodigo")]
+        public Computador ConsultarXCodigo(int Codigo)
+        {
+            clsComputador computador = new clsComputador(); 
+            return computador.Consultar(Codigo);
+        }
+        [HttpGet]
+        [Route("ConsultarTodos")]
+        public List<Computador> ConsultarTodos()
+        {
+            clsComputador computador = new clsComputador();
+            return computador.ConsultarTodos();
+        }
+        [HttpPost]
+        [Route("Insertar")]
+        public string Insertar([FromBody] Computador Computador)
+        {
+            clsComputador computador = new clsComputador();
+            computador.compu = Computador;
+            return computador.Insertar();
+        }
+        [HttpPut]
+        [Route("Actualizar")]
+        public string Actualizar([FromBody] Computador Computador)
+        {
+            clsComputador computador = new clsComputador();
+            computador.compu = Computador;
+            return computador.Actualizar();
+        }
+        [HttpDelete]
+        [Route("EliminarXCodigo")]
+        public string EliminarXCodigo(int Codigo)
+        {
+            clsComputador computador = new clsComputador();
+            return computador.Eliminar(Codigo);
+        }
+    }
+}
